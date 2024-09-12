e76aecec72d0d79ff8e53cf794a068dfd6ad5aa8
JSD <js.dohil@gmail.com>
2024-09-12 14:27:57 -0700

    script file working

--- script.js
+++ script.js
@@ -1,6 +1,22 @@
-const myModal = document.getElementById('myModal')
-const myInput = document.getElementById('myInput')
-
-// myModal.addEventListener('shown.bs.modal', () => {
-//   myInput.focus()
-// })
+const cartEl = document.querySelectorAll('.card');
+const saleitems = {
+    titles:['bag','shoes'],
+    imgurl:['https://img.freepik.com/free-vector/realistic-horizontal-sale-banner-template-with-photo_23-2149017940.jpg']
+    prices:[10,50]
+};
+const cartItms = {
+    titles:[],
+    prices:[]
+};
+for (const Elmt of cartEl){
+    
+    Elmt.addEventListener('click',function(e){
+        const element = e.target;
+        if(element.matches('a')){
+            const index  = element.getAttribute('data-index');
+            cartItms.titles.push(saleitems.titles[index]);
+            cartItms.prices.push(saleitems.prices[index]);
+        }
+        console.log(cartItms);
+    })
+}
