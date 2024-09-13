1c39f9ed77e7b8d870732a10dd6e7ee3dfd5b88d
JSD <js.dohil@gmail.com>
2024-09-13 11:29:39 -0700

    login/registarion toggle

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 const cartEl = document.querySelectorAll('.card');
 const saleitems = {
     titles:['bag','shoes'],
-    imgurl:['https://img.freepik.com/free-vector/realistic-horizontal-sale-banner-template-with-photo_23-2149017940.jpg']
+    imgurl:['https://img.freepik.com/free-vector/realistic-horizontal-sale-banner-template-with-photo_23-2149017940.jpg'],
     prices:[10,50]
 };
 const cartItms = {
@@ -20,3 +20,15 @@ for (const Elmt of cartEl){
         console.log(cartItms);
     })
 }
+// toggle between Login and Registration
+
+const signupbtn = document.querySelector('#signup');
+const loginbtn = document.querySelector('#login');
+signupbtn.addEventListener('click',function(){
+    signupbtn.parentElement.classList.toggle('invisible');
+    loginbtn.parentElement.classList.toggle('invisible');
+})
+loginbtn.addEventListener('click',function(){
+    loginbtn.parentElement.classList.toggle('invisible');
+    signupbtn.parentElement.classList.toggle('invisible');
+})
